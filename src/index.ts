1267053551fb34af81c70254a2b685dbf53abefd
Josh Murr <joshuadavidmurr@gmail.com>
2022-03-03 13:59:45 +0000

    GUI

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,6 @@ import { MnistData } from './data.js'
 
 import './styles.scss'
 
-type LayerFilters = { [k: string]: Array<tf.Tensor[]> }
-
 async function showExamples(data: MnistData) {
   // Get the examples
   const examples = data.nextTestBatch(20)
@@ -34,54 +32,16 @@ async function showExamples(data: MnistData) {
   }
 }
 
-async function renderKernels(filterLayers: LayerFilters) {
-  const renderDiv = document.getElementById('kernels')
-
-  for (const layerName in filterLayers) {
-    const filterArray = filterLayers[layerName]
-    const title = document.createElement('h4')
-    title.innerText = layerName
-    renderDiv.appendChild(title)
-    for (const f of filterArray) {
-      const row = document.createElement('div')
-      for (const k of f) {
-        //const rk = tf.image.resizeNearestNeighbor(k.squeeze(-1), [28,28])
-        const [w, h, d, _] = k.shape
-        const c = document.createElement('canvas')
-        c.width = w
-        c.height = h
-        const ctx = c.getContext('2d')
-
-        const img = new ImageData(w, h)
-
-        const data = k.dataSync()
-
-        for (let x = 0; x < w; x++) {
-          for (let y = 0; y < h; y++) {
-            const ix = (y * w + x) * 4
-
-            const iv = y * w + x
-            img.data[ix + 0] = Math.floor(127 * (data[iv] + 1))
-            img.data[ix + 1] = Math.floor(127 * (data[iv] + 1))
-            img.data[ix + 2] = Math.floor(127 * (data[iv] + 1))
-            img.data[ix + 3] = 255
-          }
-        }
-
-        ctx.putImageData(img, 0, 0)
-        row.appendChild(c)
-      }
-      renderDiv.appendChild(row)
-    }
-  }
-}
-
 async function train(model: CNN, data: MnistData, gui: GUI) {
   const onEpochEnd = async (epoch: number, logs: tf.Logs) => {
-    console.log('Rendering...')
-    //const layers: Array<tf.layers.Layer> = await model.getLayers()
-    //const filters = await model.getFilters(layers)
-    await gui.update(model)
+    if (epoch % 10 === 0) {
+      console.log('Rendering...')
+      await gui.update(model)
+    }
+  }
+
+  const onTrainEnd = () => {
+    console.log('Finito')
   }
 
   const BATCH_SIZE = 8 // 512
@@ -101,9 +61,9 @@ async function train(model: CNN, data: MnistData, gui: GUI) {
   return model.model.fit(trainXs, trainYs, {
     batchSize: BATCH_SIZE,
     validationData: [testXs, testYs],
-    epochs: 5,
+    epochs: 1,
     shuffle: true,
-    callbacks: { onEpochEnd },
+    callbacks: { onEpochEnd, onTrainEnd },
   })
 }
 
@@ -113,17 +73,11 @@ async function run() {
 
   const gui = new GUI()
 
-  //const renderDiv = document.createElement('div')
-  //renderDiv.id = 'kernels'
-  //document.body.appendChild(renderDiv)
-
   //await showExamples(data);
-  //const model = getModel()
 
   const model = new CNN()
 
   await gui.init(model)
-
   await train(model, data, gui)
 }
 
